32944b71ca0777d2cf9cb4d78eb3dd46a3051df3
Oddeecc <98522124+OrangeLyx@users.noreply.github.com>
2026-09-11 00:55:07 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        dp[0][0]=0;
+        dp[0][1]=INT_MIN/2;
+        for(int i=0;i<n;i++){
+            dp[i+1][1]=max(dp[i][1],dp[i][0]-prices[i]);
+            dp[i+1][0]=max(dp[i][0],dp[i][1]+prices[i]);
+        }
+        return dp[n][0];
+    }
+};
